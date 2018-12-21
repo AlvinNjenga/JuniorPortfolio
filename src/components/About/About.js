c63c0eae89c420f60350fcd354b7a24d8afd169c
Alvin N. Retallack <alvinnjenga@gmail.com>
2018-12-21 13:54:59 +0000

    Added new pages and functionalities

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import '../../css/About.css';
+import Zoom from 'react-reveal/Zoom';
 
 class About extends Component {
   render() {
     return (
       <section id="about">
-        <div className="about-content">
-          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident aliquam itaque, ab optio, omnis cupiditate ut accusamus alias, quae vitae ducimus animi autem aperiam qui in asperiores dolorum? Adipisci, sit!</div>
-          <div>Dolorem nesciunt enim odit, voluptate maxime corporis, odio assumenda provident neque dolor atque minima ad! At rem laborum ullam deserunt in est veniam asperiores corrupti, ea optio, magni fugiat nulla.</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg title-wrapper">
+              <Zoom>
+                <h1>ABOUT</h1>
+              </Zoom>
+              <div className="title-underline" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <i className="fa fa-rebel" />
+            </div>
+            <div className="col-sm">
+              <p>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                  "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+                  What is Lorem Ipsum?
+              </p>
+            </div>
+            <div className="col-sm">
+              <p>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                  "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+                  What is Lorem Ipsum?
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     );
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import '../css/App.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './Navbar/Navbar';
 import HomePage from './Home/HomePage';
 import About from './About/About';
+import Projects from './Projects/Projects';
+
+library.add(faBookOpen);
 
 class App extends Component {
   render() {
@@ -12,6 +19,7 @@ class App extends Component {
         <HomePage />
         <Navbar />
         <About />
+        <Projects />
       </div>
     );
   }
